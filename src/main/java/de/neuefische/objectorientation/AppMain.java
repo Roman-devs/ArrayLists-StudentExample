40c278942a0168a91a0c37f82f24c5b68fc14abe
Roman Kuite <roman.kuite@gmail.com>
2021-01-28 15:21:43 +0100

    Added Hash Map

--- src/main/java/de/neuefische/objectorientation/AppMain.java
+++ src/main/java/de/neuefische/objectorientation/AppMain.java
@@ -4,6 +4,8 @@ import de.neuefische.objectorientation.db.StudentDb;
 import de.neuefische.objectorientation.model.Student;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AppMain {
 
@@ -11,34 +13,33 @@ public class AppMain {
     public static void main(String[] args) {
 
         //
-        StudentDb studentDataBase = new StudentDb(new ArrayList<>());
+        StudentDb studentDataBase = new StudentDb(new HashMap<>());
         Student student1 = new Student("1", "Frank", "Sport");
         Student student2 = new Student("2", "Horst", "Tabletennis");
         Student student3 = new Student("3", "Hartmut", "Dance");
         Student student4 = new Student("4", "Gandalf", "Witchcraft");
         Student student5 = new Student("5", "Voldemort", "Magic");
         //
-        studentDataBase.add(student1);;
-        studentDataBase.add(student2);;
-        studentDataBase.add(student3);;
-        studentDataBase.add(student4);;
-        studentDataBase.add(student5);;
+        studentDataBase.put(student1.getId(), student1);;
+        studentDataBase.put(student2.getId(), student1);;
+        studentDataBase.put(student3.getId(), student1);;
+        studentDataBase.put(student4.getId(), student1);;
+        studentDataBase.put(student5.getId(), student1);;
         // Print All Students included in StudentDataBase before deletion
-        printAllStudents(studentDataBase);
-        // Select ID to be removed
-        String toBeDeleted = "4";
-        // Remove Student from ArrayList StudentDataBase
-        studentDataBase.remove(toBeDeleted);
-        // Print Out the new modified version of ArrayList StudentDataBase
-        System.out.println(" ");
-        System.out.println("After Deletion of ID" + " " + toBeDeleted+":");
-        System.out.println(" ");
-        printAllStudents(studentDataBase);
+//        printAllStudents(studentDataBase);
+//        // Select ID to be removed
+//        String toBeDeleted = "4";
+//        // Remove Student from ArrayList StudentDataBase
+//        studentDataBase.remove(toBeDeleted);
+//        // Print Out the new modified version of ArrayList StudentDataBase
+//        System.out.println(" ");
+//        System.out.println("After Deletion of ID" + " " + toBeDeleted+":");
+//        System.out.println(" ");
 
     }
-    private static void printAllStudents(StudentDb studentDataBase) {
-        for (Student student : studentDataBase.list()) {
-            System.out.println("ID: " + student.getId() + " " +student.getName() + " " + "Subject:"  + " " + student.getSubject());
-        }
-    }
+//    private static void printAllStudents(StudentDb studentDataBase) {
+//        for (Student student : studentDataBase.getValue()) {
+//            System.out.println("ID: " + student.getId() + " " +student.getName() + " " + "Subject:"  + " " + student.getSubject());
+//        }
+//    }
 }
